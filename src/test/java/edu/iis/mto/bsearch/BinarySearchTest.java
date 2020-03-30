509eead73268a21ca5d0a49819fcc0b2fed33bf9
Miretpl <miretpl@gmail.com>
2020-03-31 01:40:46 +0200

    testy_v2 dodanie biblioteki hamcrest

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,9 +1,12 @@
 package edu.iis.mto.bsearch;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BinarySearchTest {
 
@@ -14,68 +17,69 @@ class BinarySearchTest {
     static final int[] oneElementSeq = {1};
     static final int[] manyPositiveElementsSeq = {2, 3, 4, 5, 6};
     static final int[] manyNegativeElementsSeq = {-20, -15, -10, -5, 0};
+
     static final BinarySearch binarySearch = BinarySearch.create();
 
     @Test public void searchKeyInOneElementSeqWithKeyInIt() {
         int key = 1;
         var searchResult = binarySearch.search(key, oneElementSeq);
 
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(oneElementSeq[searchResult.getPosition()], key);
+        assertTrue(searchResult.isFound());
+        assertThat(key, is(oneElementSeq[searchResult.getPosition()]));
     }
 
     @Test public void searchKeyInOneElementSeqWithoutKeyInIt() {
         int key = 2;
         var searchResult = binarySearch.search(key, oneElementSeq);
 
-        Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(searchResult.getPosition(), ELEMENT_NOT_FOUND);
+        assertFalse(searchResult.isFound());
+        assertThat(ELEMENT_NOT_FOUND, is(searchResult.getPosition()));
     }
 
     @Test public void searchFirstKeyInManyPositiveElementSeqWithoutKeyInIt() {
         int key = 2;
         var searchResult = binarySearch.search(key, manyPositiveElementsSeq);
 
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(manyPositiveElementsSeq[searchResult.getPosition()], key);
+        assertTrue(searchResult.isFound());
+        assertThat(key, is(manyPositiveElementsSeq[searchResult.getPosition()]));
     }
 
     @Test public void searchLastKeyInManyPositiveElementSeqWithoutKeyInIt() {
         int key = 6;
         var searchResult = binarySearch.search(key, manyPositiveElementsSeq);
 
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(manyPositiveElementsSeq[searchResult.getPosition()], key);
+        assertTrue(searchResult.isFound());
+        assertThat(key, is(manyPositiveElementsSeq[searchResult.getPosition()]));
     }
 
     @Test public void searchMiddleKeyInManyPositiveElementSeqWithoutKeyInIt() {
         int key = 4;
         var searchResult = binarySearch.search(key, manyPositiveElementsSeq);
 
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(manyPositiveElementsSeq[searchResult.getPosition()], key);
+        assertTrue(searchResult.isFound());
+        assertThat(key, is(manyPositiveElementsSeq[searchResult.getPosition()]));
     }
 
     @Test public void searchMissingKeyInManyPositiveElementSeqWithoutKeyInIt() {
         int key = 8;
         var searchResult = binarySearch.search(key, manyPositiveElementsSeq);
 
-        Assertions.assertFalse(searchResult.isFound());
-        Assertions.assertEquals(searchResult.getPosition(), ELEMENT_NOT_FOUND);
+        assertFalse(searchResult.isFound());
+        assertThat(ELEMENT_NOT_FOUND, is(searchResult.getPosition()));
     }
 
     @Test public void searchMiddleKeyInManyNegativeElementSeqWithoutKeyInIt() {
         int key = -10;
         var searchResult = binarySearch.search(key, manyNegativeElementsSeq);
 
-        Assertions.assertTrue(searchResult.isFound());
-        Assertions.assertEquals(manyNegativeElementsSeq[searchResult.getPosition()], key);
+        assertTrue(searchResult.isFound());
+        assertThat(key, is(manyNegativeElementsSeq[searchResult.getPosition()]));
     }
 
     @Test public void searchKeyInEmptySeq() {
         int key = 10;
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             binarySearch.search(key, emptySeq);
         });
     }
@@ -83,7 +87,7 @@ class BinarySearchTest {
     @Test public void searchKeyInNullSeq() {
         int key = 10;
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(IllegalArgumentException.class, () -> {
             binarySearch.search(key, nullSeq);
         });
     }
